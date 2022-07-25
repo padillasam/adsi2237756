8f3cadba547802af10e51af38c7105886ae74979
padillasam <panarick@gmail.com>
2022-07-25 17:27:16 -0500

    variables, contantes y operadores aritméticos

--- intro/inicio.js
+++ intro/inicio.js
@@ -1,4 +1,19 @@
 console.log("Me apasiona el desarrollo de software");
 var x=100;
 var y=200;
-console.log(`${x} + ${y} = ${x+y}`)
+console.log(`${x} + ${y} = ${x+y}`);
+//Ejemplo let, var y const
+var a=100;
+let b=20;
+const c=1;
+console.log(`Valor de a= ${a}`);
+console.log(`Valor de b= ${b}`);
+console.log(`Valor de c= ${c}`);
+//Cambio de valores
+a="cambio de numero por texto";
+b=4,6;
+//c=12; //no se puede porque c es const
+console.log(`Valor de a= ${a}`);
+console.log(`Valor de b= ${b}`);
+console.log(`Valor de c= ${c}`);
+
---
+++ intro/operadores.js
@@ -0,0 +1,9 @@
+//Operadores Aritméticos
+var x=4323,y=2357;
+console.log(`Suma= ${x+y}`);
+console.log(`Resta= ${x-y}`)
+console.log(`Multiplicacion = ${x*y}`)
+console.log(`División = ${x/y}`)
+console.log(`Modulo= ${x%y}`)
+console.log(`Raiz de x = ${Math.sqrt(x)}`)
+console.log(`X elevado a la 3= ${Math.pow(x,3)}`)
